a625515391cd3be759be6de0fd60837eaddbb4dd
jtagcat <git-12dbd862@jtag.cat>
2022-06-01 04:23:45 +0300

    a

--- focusline.go
+++ focusline.go
@@ -3,131 +3,136 @@ package main
 import (
 	"bufio"
 	"fmt"
-	"os"
-	"strconv"
+	"io"
 	"strings"
-
-	"github.com/urfave/cli/v2"
 )
 
-func main() {
-	app := &cli.App{
-		Name:    "focusline",
-		Version: "0.1.0",
-		Authors: []*cli.Author{{Name: "jtagcat"}}, // TODO: Email: ""
+// func main() {
+// 	app := &cli.App{
+// 		Name:    "focusline",
+// 		Version: "0.1.0",
+// 		Authors: []*cli.Author{{Name: "jtagcat"}}, // TODO: Email: ""
 
-		Description: "Center text aiming at nth character.",
-		Usage:       "focusline <focus> <stdin",
-		Flags: []cli.Flag{
-			&cli.IntFlag{
-				Name:     "wrap",
-				Aliases:  []string{"w"},
-				Usage:    "per-line character limit",
-				Required: false,
-			},
-			&cli.IntFlag{
-				Name:     "first",
-				Aliases:  []string{"f"},
-				Usage:    "character limit on first line (focus is relative to 1st)",
-				Required: false,
-			},
-			&cli.IntFlag{
-				Name:     "shift",
-				Aliases:  []string{"start", "s"},
-				Usage:    "shift focus by n on first line",
-				Required: false,
-			},
-		},
-		EnableBashCompletion: true,
+// 		Description: "Center text aiming at nth character.",
+// 		Usage:       "focusline <focus> <stdin",
+// 		Flags: []cli.Flag{
+// 			&cli.IntFlag{
+// 				Name:     "wrap",
+// 				Aliases:  []string{"w"},
+// 				Usage:    "per-line character limit",
+// 				Required: false,
+// 			},
+// 			// center; except last: left-align, right-align
+// 		},
+// 		EnableBashCompletion: true,
 
-		Action: func(c *cli.Context) error {
-			args := c.Args()
-			if args.Len() != 1 {
-				return fmt.Errorf("expected exactly 1 argument, got %d", args.Len())
-			}
+// 		Action: func(c *cli.Context) error {
+// 			args := c.Args()
+// 			if args.Len() != 1 {
+// 				return fmt.Errorf("expected exactly 1 argument, got %d", args.Len())
+// 			}
 
-			focusStr := args.First()
-			focus, err := strconv.Atoi(focusStr)
-			if err != nil {
-				return fmt.Errorf("argument focusChar must be an integer")
-			}
+// 			focusStr := args.First()
+// 			focus, err := strconv.Atoi(focusStr)
+// 			if err != nil {
+// 				return fmt.Errorf("argument focusChar must be an integer")
+// 			}
 
-			fShift := c.Int("shift")
-			fFirst := c.Int("first")
-			fWrap := c.Int("wrap")
-			if fShift > 0 && fWrap+fFirst == 0 {
-				return fmt.Errorf("using shift without wrapping is ineffective;\n" +
-					"to use shift, use a character limit (wrap or first)")
-			}
+// 			fWrap := c.Int("wrap")
+// 			if fWrap <= focus {
+// 				return fmt.Errorf("flag \"wrap\" (%d) must be a larger value than focus (%d)", fWrap, focus)
+// 			}
 
-			// scanner, once := bufio.NewScanner(os.Stdin), false // TODO:
-			xyz, _ := os.Open("test")
-			scanner, once := bufio.NewScanner(xyz), false
-			for i := 1; scanner.Scan(); i++ {
-				txt := scanner.Text()
-				var line []string
+// 			// scanner, once := bufio.NewScanner(os.Stdin), false // TODO:
+// 			xyz, _ := os.Open("test")
 
-				doFirstWrap := i == 1 && fFirst != 0
-				if doFirstWrap {
-					line = append(line, txt[:fFirst])
-					txt = txt[fFirst:]
-				}
+// 			return nil
+// 		},
+// 	}
+// 	err := app.Run(os.Args)
+// 	if err != nil {
+// 		fmt.Fprintln(os.Stderr, "error:", err.Error())
+// 		os.Exit(1)
+// 	}
+// }
 
-				if fWrap != 0 {
-					line = append(line, StringsSplitEveryN(txt, fWrap)...)
-				}
-				for i, l := range line {
-					if i+1 == len(line) {
-						output(FocusText(l, focus))
-					} else {
-						output(l)
-					}
+// func output(line string) {
+// 	fmt.Println(line)
+// }
 
-					if i == 1 && fShift != 0 && !once {
-						focus = focus - fShift
-						once = true // sync.Once got way too complex and unreadable
-					}
-				}
-			}
-
-			if err := scanner.Err(); err != nil {
-				return fmt.Errorf("while reading standard input: %w", err)
-			}
-
-			return nil
-		},
+// TODO:
+//
+// mode: -1: focus all lines, prefer left
+// mode:  0: focus all lines, prefer right
+// mode:  1: left-align all lines, focus last line, prefer left
+// mode:  2: right-align all lines, focus last line, prefer right
+func FocusReader(r io.Reader, focus, wrap uint, mode int) (w []string, _ error) {
+	var prefBool bool
+	switch mode {
+	case -1, 1:
+		prefBool = true
+	case 0, 2:
+		prefBool = false
+	default:
+		return w, fmt.Errorf("runtime enum: FocusReader: mode %d is invalid", mode)
 	}
-	err := app.Run(os.Args)
-	if err != nil {
-		fmt.Fprintf(os.Stderr, "error: %s", err.Error())
-	}
-}
 
-func output(line string) {
-	fmt.Println(line)
-}
-
-func StringsSplitEveryN(s string, n int) (o []string) {
-	for i := 0; i < len(s); i += n {
-		if n > len(s)-i {
-			// last (unfinished) piece
-			n = len(s) - i
+	scanner := bufio.NewScanner(r)
+	for i := 1; scanner.Scan(); i++ {
+		// line := []string{scanner.Text()}
+		line := StringsSplitEveryN(scanner.Text(), wrap)
+		for _, l := range line {
+			w = append(w, Focus(l, focus, wrap, prefBool))
 		}
-		o = append(o, s[i:i+n])
+		// if fWrap != 0 {
+		// 	line = StringsSplitEveryN(line[0], fWrap)
+		// }
+		// for _, l := range line {
+		// 	output(Focus(l, focus, false))
+		// }
+	}
+
+	if err := scanner.Err(); err != nil {
+		return w, fmt.Errorf("while reading input: %w", err)
+	}
+	return w, nil
+}
+
+// Splits string to string slice after every n characters.
+func StringsSplitEveryN(s string, n uint) (o []string) {
+	if n == 0 {
+		return []string{s}
+	}
+	u := int(n)
+	for i := 0; i < len(s); i += u {
+		if u > len(s)-i {
+			// last (unfinished) piece
+			u = len(s) - i
+		}
+		o = append(o, s[i:i+u])
 	}
 	return
 }
 
-// Centers string by prepending whitespace
-//
-// "      hello" when forced unsymetrical,
-// "       tere" text shifted to right
-// target:  ^
-func FocusText(text string, focus int) string {
-	textBeforeFocus := (len(text) / 2) + (len(text) % 2)
-	whitespace := focus - textBeforeFocus
+// Centers string by prepending whitespace |  ↓  | |  ↓  |
+// maxLen is a soft limit for whitespaces  |focus| |focus|
+// "      hello" when forced unsymetrical, |  on | | on  |
+// "       tere" text shifted to right or  | left| |rigt |
+// target:  ^^    left (preferLeft=true)   |  /  | |  \  |
+//          LR                             | /   | |   \ |
+func Focus(text string, focus, maxLen uint, preferLeft bool) string {
+	// TODO: check input sanity; is maxLen right place?
+	textBeforeFocus := (len(text) / 2) + 1
+	if (len(text)%2) == 0 && preferLeft {
+		textBeforeFocus -= 1
+	}
+
+	whitespace := int(focus) - textBeforeFocus
 
 	if whitespace > 0 {
+		if maxLen > 0 && (len(text)+whitespace) >= int(maxLen) {
+			whitespace = int(maxLen) - len(text)
+		}
 		return strings.Repeat(" ", whitespace) + text
 	}
 	return text
---
+++ focusline_test.go
@@ -0,0 +1,69 @@
+package main
+
+import (
+	"strings"
+	"testing"
+
+	"github.com/stretchr/testify/assert"
+)
+
+func TestFocusNothing(t *testing.T) {
+	t.Parallel()
+	for _, x := range []bool{false, true} {
+		assert.Equal(t, "123", Focus("123", 2, 0, x))
+	}
+}
+
+func TestFocusEqual(t *testing.T) {
+	t.Parallel()
+	for _, x := range []bool{false, true} {
+		assert.Equal(t, " 123", Focus("123", 3, 0, x))
+		assert.Equal(t, "  x", Focus("x", 3, 0, x))
+	}
+}
+
+func TestFocusLeft(t *testing.T) {
+	t.Parallel()
+	assert.Equal(t, " xx", Focus("xx", 2, 0, true))
+}
+
+func TestFocusRight(t *testing.T) {
+	t.Parallel()
+	assert.Equal(t, "xx", Focus("xx", 2, 0, false))
+}
+
+//
+
+func TestFocusLineM1_5Wrap0(t *testing.T) {
+	t.Parallel()
+
+	in := strings.NewReader("0\n" +
+		"12345\n" +
+		"1234\n" +
+		"123456\n")
+	out, err := FocusReader(in, 3, 0, -1)
+	assert.Nil(t, err)
+	assert.Equal(t, []string{
+		"  0",
+		"12345",
+		" 1234",
+		"123456",
+	},
+		out)
+}
+
+func TestFocusLineM1_4Wrap6(t *testing.T) {
+	t.Parallel()
+
+	in := strings.NewReader("0\n" +
+		"123456\n" +
+		"12345678\n")
+	out, err := FocusReader(in, 4, 6, -1)
+	assert.Nil(t, err)
+	assert.Equal(t, []string{
+		"   0",
+		"123456",
+		"123456", "   78",
+	},
+		out)
+}
